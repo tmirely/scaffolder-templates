fbd5734e542c8a1995522d22d93915dd3a766780
tmirely <tmirely@thoughtworks.com>
2023-02-01 14:56:06 -0300

    Create a archetype and template to springboot scaffolder

---
+++ API-springboot-mysql/archetype/src/main/java/com/poc/apiarchetype/API/Archetype/ApiArchetypeApplication.java
@@ -0,0 +1,13 @@
+package com.poc.apiarchetype.API.Archetype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiArchetypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiArchetypeApplication.class, args);
+	}
+
+}
---
+++ API-springboot-mysql/archetype/src/test/java/com/poc/apiarchetype/API/Archetype/ApiArchetypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.apiarchetype.API.Archetype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiArchetypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
